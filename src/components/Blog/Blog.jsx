c96ecf8429d0a5db680a1cb760c0cb33982a5ad4
Juwel-771 <juwelah99@gmail.com>
2025-03-01 15:50:46 +0600

    bookmark button added

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,12 +1,13 @@
 
 import { prototype } from "postcss/lib/previous-map";
 import PropTypes from "prop-types";
+import { IoBookmark } from "react-icons/io5";
 
 const Blog = ({blog}) => {
     const {title, cover, author, author_img, posted_date, reading_time, hashtags} = blog; 
     return (
-        <div>
-            <img src={cover}  alt={`Cover picture of the title ${title}`} />
+        <div className="mb-20">
+            <img className="w-full" src={cover}  alt={`Cover picture of the title ${title}`} />
             <div className="flex justify-between">
                 <div className="flex ml-2">
                     <img className="w-14" src={author_img} alt="" />
@@ -17,6 +18,7 @@ const Blog = ({blog}) => {
                 </div>
                 <div>
                     <span> {reading_time} min read</span>
+                    <button className="ml-2 text-2xl text-red-600"> <IoBookmark /> </button>
                 </div>
             </div>
             <h2 className="text-4xl">{title}</h2>
--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Bookmarks = () => {
     return (
         <div className='md: w-1/3'>
-            <h3>Bookmarks components</h3>
+            <h3>Bookmarks</h3>
         </div>
     );
 };
