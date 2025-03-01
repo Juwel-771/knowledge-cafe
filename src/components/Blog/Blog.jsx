2636ca049acb221b2a76792ee0a985a62569e2b1
Juwel-771 <juwelah99@gmail.com>
2025-03-01 15:08:09 +0600

    blog post created

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -3,10 +3,26 @@ import { prototype } from "postcss/lib/previous-map";
 import PropTypes from "prop-types";
 
 const Blog = ({blog}) => {
-    const {title} = blog; 
+    const {title, cover, author, author_img, posted_date, reading_time, hashtags} = blog; 
     return (
         <div>
+            <img src={cover}  alt={`Cover picture of the title ${title}`} />
+            <div className="flex justify-between">
+                <div className="flex ml-2">
+                    <img className="w-14" src={author_img} alt="" />
+                    <div>
+                        <h3 className="text-2xl"> {author} </h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span> {reading_time} min read</span>
+                </div>
+            </div>
             <h2 className="text-4xl">{title}</h2>
+            {
+                hashtags.map((hash, idx) => <span key={idx}><a href="">{hash} </a></span>)
+            }
         </div>
     );
 };
